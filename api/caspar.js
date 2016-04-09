7c5d7d8eee17ac75e85ec91481166aa4cd4a76e4
Ossi Pesonen <ossi@ossipesonen.fi>
2016-04-09 13:57:09 +0300

    Commented routes and code. Changed error codes.

--- api/caspar.js
+++ api/caspar.js
@@ -1,14 +1,14 @@
 /**
  * Routes for CasparCG
  *
- * Includes routes test
+ * Includes routes connect, disconnect, play, stop
  *
  * Function parameters:
  *  - router. Express router object
  *  - connection. MySQL connection object for queries
  *
  * Required:
- *  - casparcg      CasparCG Node.js library (https://github.com/respectTheCode/node-caspar-cg) for communication
+ *  - caspar-cg      CasparCG Node.js library (https://github.com/respectTheCode/node-caspar-cg) for communication
  *
  */
 
@@ -16,6 +16,9 @@ var CasparCG = require('caspar-cg');
 var ccg = new CasparCG("192.168.1.100", 5250);
 var _cg_connected = false;
 
+/**
+ * Helper function for connection because it's used multiple times
+ */
 function _connect(callback) {
     ccg.connect(function () {
         ccg.on('connected', function () {
@@ -36,6 +39,9 @@ function _connect(callback) {
     });
 }
 
+/**
+ * Helper function for disconnection
+ */
 function _disconnect() {
     ccg.disconnect();
     _cg_connected = false;
@@ -43,44 +49,89 @@ function _disconnect() {
 }
 
 module.exports = function(router, connection) {
+
+    /**
+     * Route: connect
+     * Path: /api/casparcg/connect/
+     *
+     * Connects to CasparCG server
+     *
+     * Returns either a success code (200), or an error code 503 and message.
+     */
     router.post('/casparcg/connect', function(req, res) {
         _connect(function(error) {
             if(_cg_connected) {
                 res.status(200).json({success: true, message: 'Connection successful'});
             } else {
-                res.status(401).json({success: false, message: error});
+                res.status(503).json({success: false, message: error});
             }
         });
     });
 
+    /**
+     * Route: disconnect
+     * Path: /api/casparcg/disconnect/
+     *
+     * Disconnects from CasparCG server
+     *
+     * Returns a success code (200) and message
+     */
     router.post('/casparcg/disconnect', function(req, res) {
         _disconnect();
         return res.status(200).json({success: true, message: 'Disconnected from server'});
     });
 
+    /**
+     * Route: play
+     * Path: /api/casparcg/play/
+     *
+     * Plays a given template in given channel passing on data from inputs as object
+     * Request body example: { channel: '1-1', template: 'information', inputs: { f0: 'First', f1: 'Second', f2: 'Third } }
+     *
+     */
     router.post('/casparcg/play', function(req, res) {
         var fields = req.body;
 
+        // Has to be changed to handle not connected -> reconnect. Experienced problems on first test
         if(!_cg_connected) {
-            return res.status(300).json({success: false, message: 'Not connected to server'});
+            return res.status(503).json({success: false, message: 'Not connected to server'});
         }
 
         ccg.loadTemplate(fields.channel, 'JS/' + fields.template, 1, fields.inputs);
     });
 
+    /**
+     * Route: stop
+     * Path: /api/casparcg/stop/
+     *
+     * Stops a template in given channel
+     * Request body example: { channel: '1-1' }
+     *
+     */
     router.post('/casparcg/stop', function() {
+        // Has to be changed to handle not connected -> reconnect. Experienced problems on first test
         if(!_cg_connected) {
-            return false;
+            return res.status(503).json({success: false, message: 'Not connected to server'});
         }
 
+        // Change this to get channel from request
         ccg.stopTemplate('1-1');
     });
 
+    /**
+     * Route: update
+     * Path: /api/casparcg/update/
+     *
+     * Updates a template in given channel with given inputs
+     * Request body example: { channel: '1-1', inputs: { f0: 'First', f1: 'Second', f2: 'Third } }
+     *
+     */
     router.put('/casparcg/update', function(req, res) {
         var fields = req.body;
 
+        // Has to be changed to handle not connected -> reconnect. Experienced problems on first test
         if(!_cg_connected) {
-            return res.status(300).json({success: false, message: 'Not connected to server'});
+            return res.status(503).json({success: false, message: 'Not connected to server'});
         }
 
         ccg.updateTemplateData('1-1', fields.inputs);
